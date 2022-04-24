6011fdeb5e971ce4d125bd15e1eaad12a4d0a7b9
Florian Brandes <florian.brandes@posteo.de>
2022-04-24 23:25:16 +0200

    add more code
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- src/nix-cbm/__init__.py
+++ src/nix-cbm/__init__.py
@@ -1,10 +1,26 @@
-from . import check_tools
+import logging
+import sys
+
+import check_tools
+
+logging.basicConfig(level=logging.DEBUG)
 
 
-def main() -> str:
-    return "Hello world!"
+def main() -> None:
+    _preflight()
+
+
+def _preflight() -> bool:
+    """
+    Check if all running conditions are met.
+    OUTPUT: ok, bool.
+    """
+    logging.debug("running preflight checklist")
+    missing_programs = check_tools.checkTools()
+    if missing_programs:
+        sys.exit("The following programs are missing: " + str(missing_programs))
+    return True
 
 
 if __name__ == "__main__":
-    print(main())
-    check_tools.checkTools()
+    main()
