d19687d001d9200fbb28cae089b359fd5cf9096f
Florian Brandes <florian.brandes@posteo.de>
2022-04-24 18:20:18 +0200

    add venv like shell hook
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- flake.nix
+++ flake.nix
@@ -41,7 +41,14 @@
             pkgs.mypy
             pre-commit
           ];
-          shellHook = "";
+          shellHook = ''
+            # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+            # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+            export PIP_PREFIX=$(pwd)/_build/pip_packages
+            export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+            export PATH="$PIP_PREFIX/bin:$PATH"
+            unset SOURCE_DATE_EPOCH
+          '';
         };
         package = pkgs.python3Packages.buildPythonApplication rec {
           pname = "nix-check-build-merge";
