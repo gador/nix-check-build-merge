5382eae64d7a6b1744da880481078179859a637e
Florian Brandes <florian.brandes@posteo.de>
2022-04-28 12:55:48 +0200

    added pycharm .idea folder added more functions
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- src/nix-cbm/__init__.py
+++ src/nix-cbm/__init__.py
@@ -1,5 +1,8 @@
 import logging
+import os
+import subprocess
 import sys
+import tempfile
 
 import checks
 import git
@@ -7,35 +10,69 @@ import git
 logging.basicConfig(level=logging.DEBUG)
 
 
-def _preflight() -> bool:
+def _preflight(nixpkgs_path: str) -> bool:
     """
     Check if all running conditions are met.
+    Also, cd to the nixpkgs dir.
     OUTPUT: ok, bool.
     """
     logging.debug("running preflight checklist")
     missing_programs = checks.check_tools()
     if missing_programs:
         sys.exit("The following programs are missing: " + str(missing_programs))
-    checks.check_nixpkgs_dir()
+    checks.check_nixpkgs_dir(nixpkgs_path)
     git.git_checkout("testcommit")
     return True
 
 
-# TODO: add function
-def find_maintainer(maintainer: str) -> list[str]:
-    """ "
-    find all occurances of a given maintainer
-    INPUT: maintainer, string.
-    OUTPUT: package names, list of strings.
-    """
-    logging.info(f"Will look for {maintainer}")
-    return []
-
-
 def main() -> None:
-    _preflight()
-    packages = find_maintainer("gador")
-    logging.info(str(packages))
+    # TODO add CLI and API interface
+    nixpkgs_dir = "/home/wogan/software/git/nixpkgs"
+    maintainer = "gador"
+
+    if not _preflight(nixpkgs_dir):
+        exit(1)
+
+    nixcbm = NixCbm()
+    nixcbm.nixpkgs_repo = nixpkgs_dir
+    nixcbm.find_maintainer(maintainer)
+    logging.info(str(nixcbm.maintained_packages))
+
+
+class NixCbm:
+    """ "
+    Main class to save important state information
+    """
+
+    def __init__(self):
+        self.nixpkgs_repo = ""
+        self.maintained_packages = []
+
+    def find_maintainer(self, maintainer: str):
+        """ "
+        find all occurances of a given maintainer
+        INPUT: maintainer, string.
+        OUTPUT: package names, List of strings.
+        """
+        logging.info(f"Will look for {maintainer}")
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        cmd = [
+            "nix-shell",
+            os.path.join(current_dir, "nix", "check-maintainer.nix"),
+            "--argstr",
+            "maintainer",
+            maintainer,
+            "-I",
+            "nixpkgs=" + self.nixpkgs_repo,
+        ]
+        # stdout = subprocess.run(cmd, check=True).stdout()
+        # self.maintained_packages =
+        with tempfile.NamedTemporaryFile(mode="w") as tmp:
+            subprocess.run(cmd, stdout=tmp, check=True)
+            tmp.flush()
+            with open(tmp.name) as f:
+                stdout = f.read()
+                self.maintained_packages = stdout.split(",")
 
 
 if __name__ == "__main__":
