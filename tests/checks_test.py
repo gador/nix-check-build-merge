48b35adb1ae45e04cd4af2f5c7434d215c56d173
Florian Brandes <florian.brandes@posteo.de>
2022-04-29 14:31:55 +0200

    more tests
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- tests/checks_test.py
+++ tests/checks_test.py
@@ -1,12 +1,24 @@
 import unittest
+from unittest import mock
 import nix_cbm.checks as checks
 
 
-class MyTestCase(unittest.TestCase):
+class CheckTestCase(unittest.TestCase):
     def test_exists(self):
         self.assertFalse(checks._exists("non_existing_program"))
-        self.assertTrue(checks._exists("python")) 
+        self.assertTrue(checks._exists("python"))
 
+    def test_check_tools(self):
+        self.assertEqual(checks.check_tools(["python"]), [])
+        self.assertEqual(checks.check_tools(["non_existing_program"]), ['non_existing_program'])
+
+    @mock.patch("os.path.exists")
+    def test_check_nixpkgs_dir(self, path):
+        self.assertTrue(checks.check_nixpkgs_dir("/"))
+        assert 3 == path.call_count
+
+    def test_check_nixpkgs_dir(self):
+        self.assertRaises(LookupError)
 
 if __name__ == '__main__':
     unittest.main()
