452b060fa6652f9885e8c86e08a5cb891f1c9c90
Florian Brandes <florian.brandes@posteo.de>
2022-05-06 22:42:59 +0200

    WIP: add table to frontend
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- src/nix_cbm/frontend/__init__.py
+++ src/nix_cbm/frontend/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+import nix_cbm
 from nix_cbm.config import Config
 
 app = Flask(__name__)
@@ -10,7 +11,17 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     # this needs to be offloaded to a different worker
-    return render_template("index.html", maintainer="")
+    # also, for now maintainer is hardcoded
+    # TODO: add form to enter maintainer
+    # TODO: make DataTable static
+    packages = nix_cbm.models.Packages.query
+    if request.method == "POST":
+        if request.form.get("check") == "Check for Maintainer":
+            pass  # do something
+    elif request.method == "GET":
+        return render_template("index.html", maintainer="gador", packages=packages)
+
+    return render_template("index.html")
