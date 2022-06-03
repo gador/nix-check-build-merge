b4bfa69ebcc89a8d703638fe154ce7c92e571773
Florian Brandes <florian.brandes@posteo.de>
2022-06-03 13:30:34 +0200

    add arch to check, preflight and testing config
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- src/nix_cbm/config.py
+++ src/nix_cbm/config.py
@@ -15,5 +15,11 @@ class Config:
     NIXPKGS_WORKDIR = os.environ.get("NIXPKGS_WORKDIR") or nixpkgs_workdir
     MAINTAINER = os.environ.get("MAINTAINER") or ""
     NIXPKGS_ORIGINAL = os.environ.get("NIXPKGS") or ""
+    if os.environ.get("ARCH_TO_CHECK"):
+        ARCH_TO_CHECK = str(os.environ.get("ARCH_TO_CHECK")).split(",") 
+    else:
+        ARCH_TO_CHECK = ["x86_64-linux"]
     REDIS_URL = os.environ.get("REDIS_URL") or "redis:///"
     QUEUES = ["default"]
+    PREFLIGHT_DONE = False
+    TESTING = False
