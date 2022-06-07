eea5c45f66e1f3c9ac81d99189cf5aee901f042d
Florian Brandes <florian.brandes@posteo.de>
2022-06-07 18:19:06 +0200

    improve nixpkgs test routine
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- tests/checks_test.py
+++ tests/checks_test.py
@@ -1,4 +1,8 @@
+import os
+import tempfile
 import unittest
+from pathlib import Path
+from tempfile import mktemp
 from unittest import mock
 
 import nix_cbm.checks as checks
@@ -15,10 +19,18 @@ class CheckTestCase(unittest.TestCase):
             checks.check_tools(["non_existing_program"]), ["non_existing_program"]
         )
 
-    @mock.patch("os.path.exists")
-    def test_check_nixpkgs_dir(self, path):
-        self.assertTrue(checks.check_nixpkgs_dir("~"))
-        assert 3 == path.call_count
+    def test_check_nixpkgs_dir(self):
+        """create a temporary directory and simulate a nixpkgs"""
+        with mock.patch(
+            "os.path.expanduser"
+        ) as mock_base_path, tempfile.TemporaryDirectory() as path:
+            mock_base_path.return_value = path
+            nixpkgs_dir = os.path.join(path, "nixpkgs")
+            os.makedirs(os.path.join(nixpkgs_dir, ".git"))
+            Path(os.path.join(nixpkgs_dir, "default.nix")).touch()
+            Path(os.path.join(nixpkgs_dir, ".version")).touch()
+            self.assertTrue(checks.check_nixpkgs_dir(nixpkgs_dir))
+            mock_base_path.assert_called_once()
 
     def test_check_nixpkgs_dir_not_found(self):
         self.assertFalse(checks.check_nixpkgs_dir("~"))
